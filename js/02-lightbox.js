5a14ca3ad0c6630d72b2c1eb720114e2e30f0276
Yuliia001 <babeyuliia@gmail.com>
2023-06-27 17:03:35 +0200

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,43 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+const markup = galleryItems.map(({preview, original, description}) => `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}" 
+        alt="${description}" 
+      />
+   </a>
+</li>`).join('');
+galleryList.insertAdjacentHTML('beforeend', markup);
+
+galleryList.addEventListener('click', handlerOpenModal);
+
+
+function handlerOpenModal(evt) {
+    evt.preventDefault();
+    // console.log('target', evt.target)
+    if (evt.target.classList.contains('gallery__item')){
+        return;
+        
+    }
+    
+   
+    const lightbox = new SimpleLightbox('.gallery a', { 
+        captionType: 'alt',
+        captionDelay: 250,
+        captionPosition: 'bottom',
+        captionsData: 'alt',
+
+      });
+    lightbox.open();
+    
+    
+}
+
+
+
